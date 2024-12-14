6d0cdbe346a8ab097a778e13155a5faf6064af22
salty-max <max@jellycat.fr>
2024-12-14 16:02:19 +0100

    :recycle: refactor response typewriter animation

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -3,11 +3,14 @@ extends Control
 
 const HistoryRow := preload("res://scenes/history_row/history_row.tscn")
 
+@export var animated_text: bool = true
 @export var text_animation_speed: float = 1.0
+@export var max_history_length: int = 30
 
 @onready var history: VBoxContainer = %History
 @onready var scroll: ScrollContainer = %Scroll
 @onready var scroll_bar: VScrollBar = %Scroll.get_v_scroll_bar()
+@onready var text_input: TextInput = %TextInput
 
 var max_scroll_length := 0
 
@@ -17,13 +20,33 @@ func _ready() -> void:
 	max_scroll_length = scroll_bar.max_value
 
 func _on_text_input_text_submitted(new_text: String) -> void:
+	if new_text.is_empty():
+		return
+
 	var row := HistoryRow.instantiate()
+	row.is_text_animated = animated_text
 	row.animation_speed = text_animation_speed
-	row.set_text(new_text, "Wtf man?")
+	if animated_text:
+		row.animation_started.connect(_on_response_animation_started)
+		row.animation_finished.connect(_on_response_animation_finished)
+	row.set_text(new_text, '[color="#f38ba8"]Lorem ipsum dolor sit amet[/color], consectetur adipiscing elit. Vestibulum condimentum metus nulla, sit amet facilisis leo egestas ac. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam lacinia eget risus vitae scelerisque. Duis sagittis congue vehicula.')
 	history.add_child(row)
+
+	if history.get_child_count() >= max_history_length:
+		var rows_to_forget := history.get_child_count() - max_history_length
+		for i in range(rows_to_forget):
+			history.get_child(i).queue_free()
 
 
 func _on_scroll_bar_changed() -> void:
 	if max_scroll_length != scroll_bar.max_value:
 		max_scroll_length = scroll_bar.max_value
 		scroll.scroll_vertical = scroll_bar.max_value
+
+
+func _on_response_animation_started() -> void:
+	text_input.editable = false
+
+
+func _on_response_animation_finished() -> void:
+	text_input.editable = true
--- scenes/history_row/history_row.gd
+++ scenes/history_row/history_row.gd
@@ -1,15 +1,19 @@
 class_name HistoryRow
 extends MarginContainer
 
-@export var animation_speed: float = 2.0
+signal animation_started
+signal animation_finished
 
 @onready var response: RichTextLabel = %Response
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-var final_text: String
-var current_char_index: int = 0
-var character_delay: float = 0.0
-var elapsed_time: float = 0.0
-var is_typing: bool = false
+var is_text_animated: bool
+var animation_speed: float = 0.05
+var delay: float = 0.0
+
+
+func _ready() -> void:
+	animation_player.animation_finished.connect(_on_animation_finished)
 
 
 func set_text(cmd: String, res: String) -> void:
@@ -17,27 +21,29 @@ func set_text(cmd: String, res: String) -> void:
 		await ready
 
 	%Feedback.text = cmd
-	_start_typewriter(res)
+
+	if is_text_animated:
+		_start_typewriter(res)
+	else:
+		response.text = res
 
 
 func _start_typewriter(text: String) -> void:
-	final_text = text
-	current_char_index = 0
-	elapsed_time = 0.0
-	is_typing = true
-	character_delay = animation_speed / max(final_text.length(), 1)
-	response.clear()
+	response.text = text
+
+	# Remove bbcode from text
+	var regex = RegEx.new()
+	regex.compile(r"\[\/?[a-zA-Z0-9_=#:; ]+\]")
+	var raw_text = regex.sub(text, "", true)
+
+	var duration: float = raw_text.length() * animation_speed
+	var animation := animation_player.get_animation("typewriter")
+
+	animation.length = duration
+	animation.track_set_key_time(0, 1, duration)
+	animation_started.emit()
+	animation_player.play("typewriter")
 
 
-func _process(delta: float) -> void:
-	if is_typing:
-		elapsed_time += delta
-
-		while elapsed_time >= character_delay and current_char_index < final_text.length():
-			# Add the next character to the label
-			response.append_text(final_text[current_char_index])
-			current_char_index += 1
-			elapsed_time -= character_delay
-
-		if current_char_index >= final_text.length():
-			is_typing = false
+func _on_animation_finished(_animation_name: String) -> void:
+	animation_finished.emit()
