ba9611ddc27435cc5ea881fc68fa34bf0ebdba02
salty-max <max@jellycat.fr>
2024-12-14 14:06:18 +0100

    :sparkles: crt shader + input feedback + autoscroll

---
+++ scenes/game/game.gd
@@ -0,0 +1,26 @@
+class_name Game
+extends Control
+
+const HistoryRow := preload("res://scenes/history_row/history_row.tscn")
+
+@onready var history: VBoxContainer = %History
+@onready var scroll: ScrollContainer = %Scroll
+@onready var scroll_bar: VScrollBar = %Scroll.get_v_scroll_bar()
+
+var max_scroll_length := 0
+
+
+func _ready() -> void:
+	scroll_bar.changed.connect(_on_scroll_bar_changed)
+	max_scroll_length = scroll_bar.max_value
+
+func _on_text_input_text_submitted(new_text: String) -> void:
+	var row := HistoryRow.instantiate()
+	row.set_text(new_text, "Wtf man?")
+	history.add_child(row)
+
+
+func _on_scroll_bar_changed() -> void:
+	if max_scroll_length != scroll_bar.max_value:
+		max_scroll_length = scroll_bar.max_value
+		scroll.scroll_vertical = scroll_bar.max_value
---
+++ scenes/history_row/history_row.gd
@@ -0,0 +1,7 @@
+class_name HistoryRow
+extends MarginContainer
+
+
+func set_text(cmd: String, res: String) -> void:
+	%Feedback.text = cmd
+	%Response.text = res
